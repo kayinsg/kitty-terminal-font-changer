8746e2f3cea6ab7237f355c3447eee0ebcb0d6ce
kayinsg <kayin.gayle@gmail.com>
2025-01-01 23:58:30 -0500

    Completely Refactored Program Structure

--- fontChanger.py
+++ fontChanger.py
@@ -5,82 +5,82 @@ from pathlib import Path
 from systemFontDataExtractor import getDetailsForFontSelector
 import re as regex
 
-def fontPicker(temporaryFile):
-    fontMenu = subprocess.run(
-        ['cat', temporaryFile],
-        check=True,
-        text=True,
-        stdout=subprocess.PIPE
-    )
-    userFont = subprocess.run(
-        ['fzf'],
-        input = fontMenu.stdout,
-        check=True,
-        capture_output=True,
-        text=True
-    ).stdout.strip()
-    os.remove(temporaryFile)
-    return userFont
+
+class FontChanger:
+    def __init__(self):
+        self.kittyConfigPath = os.path.join(os.path.expanduser('~'), '.config', 'kitty', 'current_settings')
+        self.kittyConfigData = Path(self.kittyConfigPath).read_text()
 
 
-def getTemporaryFilePath():
-    workingDirectory = os.getcwd()
-    temporaryFilePath = os.path.join(workingDirectory, 'temporaryFile')
-    return temporaryFilePath
-
-def writeFontsToTemporaryFile(fonts, temporaryFilePath):
-    with open(temporaryFilePath , 'w') as file:
-        for font in fonts:
-                file.writelines(f'{font.ancestor}\n')
+    @classmethod
+    def userFontInfo(cls):
+        userChosenFont = FontMenu().letUserPickFont()
+        fontSizeByUser = input("What should the size of the font be? ")
+        return {'name': userChosenFont, 'size': fontSizeByUser}
 
 
-def getKittyConfigPath():
-    home = os.path.expanduser('~')
-    kittyConfigPath = os.path.join (home, '.config', 'kitty', 'current_settings')
-    return kittyConfigPath
+    def change(self):
+        font = self.userFontInfo()
+        self.changeFontFamily(font['name'])
+        self.changeFontSize(font['size'])
+        self.applyChangesToKittyConfig()
 
 
-def getKittyConfigData(kittyConfigPath):
-    return Path(kittyConfigPath).read_text()
+    def changeFontFamily(self, userChosenFont):
+        fontFamily = regex.compile(r'(font_family\s*)(\w+)')
+        familyMatch = fontFamily.search(self.kittyConfigData)
+        fontFamilyCategory = familyMatch.group(1)
+        fontName = familyMatch.group(2)
+        modifiedConfig = regex.sub(fontFamily, f'{fontFamilyCategory}{userChosenFont}', self.kittyConfigData)
+        self.kittyConfigData = modifiedConfig
 
 
-
-def changeFontFamily(kittyConfigData):
-    fontFamily = regex.compile(r'(font_family\s*)(\w+)')
-    familyMatch = fontFamily.search(kittyConfigData)
-    fontFamilyCategory = familyMatch.group(1)
-    fontName = familyMatch.group(2)
-    modifiedConfig = regex.sub(fontFamily, f'{fontFamilyCategory}{userChosenFont}', kittyConfigData)
-    return modifiedConfig
-
-
-def changeFontSize(kittyConfigData):
-    fontSize = regex.compile(r'(\bfont_size\s+)(\d+(\.\d+)?)')
-    fontSizeMatch = fontSize.search(kittyConfigData)
-    fontSizeCategory = fontSizeMatch.group(1)
-    actualFontSize = fontSizeMatch.group(2)
-    modifiedConfig = regex.sub(fontSize, f'{fontSizeCategory}{fontSizeByUser}', kittyConfigData)
-    return modifiedConfig
+    def changeFontSize(self, userChosenFontSize):
+        fontSize = regex.compile(r'(\bfont_size\s+)(\d+(\.\d+)?)')
+        fontSizeMatch = fontSize.search(self.kittyConfigData)
+        fontSizeCategory = fontSizeMatch.group(1)
+        actualFontSize = fontSizeMatch.group(2)
+        modifiedConfig = regex.sub(fontSize, f'{fontSizeCategory}{userChosenFontSize}', self.kittyConfigData)
+        self.kittyConfigData = modifiedConfig
 
 
-def applyChangesToKittyConfig(modifiedKittyData, kittyConfigPath):
-    with open(kittyConfigPath, "w") as config:
-        config.writelines(modifiedKittyData)
-
-
-fonts = getDetailsForFontSelector()
-temporaryFilePath = getTemporaryFilePath()
-writeFontsToTemporaryFile(fonts, temporaryFilePath)
-
-kittyConfigPath = getKittyConfigPath()
-kittyConfigData = getKittyConfigData(kittyConfigPath)
-
-modifiedFontFamily = changeFontFamily(kittyConfigData)
-modifiedFontSize = changeFontSize(modifiedFontFamily)
-
-applyChangesToKittyConfig(modifiedFontSize, kittyConfigPath)
+    def applyChangesToKittyConfig(self):
+        with open(self.kittyConfigPath, "w") as config:
+            config.writelines(self.kittyConfigData)
 
 
 
-userChosenFont = fontPicker(temporaryFilePath)
-fontSizeByUser = input("What should the size of the font be? ")
+class FontMenu:
+    def __init__(self):
+        self.fonts = getDetailsForFontSelector()
+        self.tempFile = os.path.join(os.getcwd(), 'temporaryFile')
+
+    def letUserPickFont(self):
+        self.writeFontsToTemporaryFile()
+        userSelectedFont = self.fontPicker()
+        os.remove(self.tempFile)
+        return userSelectedFont
+
+    def writeFontsToTemporaryFile(self):
+        with open(self.tempFile , 'w') as file:
+            for font in self.fonts:
+                    file.writelines(f'{font.ancestor}\n')
+
+    def fontPicker(self):
+        fontMenu = subprocess.run(
+            ['cat', self.tempFile],
+            check=True,
+            text=True,
+            stdout=subprocess.PIPE
+        )
+        userFont = subprocess.run(
+            ['fzf'],
+            input = fontMenu.stdout,
+            check=True,
+            capture_output=True,
+            text=True
+        ).stdout.strip()
+        return userFont
+
+
+FontChanger().change()
