e72c50b5bb582665fa35716eefbf10ffec3e36ac
kayinsg <kayin.gayle@gmail.com>
2025-02-26 12:20:47 -0500

    refactor: move FZFMenu to more appropriate module

--- ChangeFont.py
+++ ChangeFont.py
@@ -42,40 +42,6 @@ class FontChanger:
             config.writelines(self.kitty.data)
 
 
-
-class FontMenu:
-    def __init__(self, fontCollection):
-        self.fonts = fontCollection
-        self.tempFile = path.join(getcwd(), 'temporaryFile')
-
-    def letUserPickFont(self):
-        self.writeFontsToTemporaryFile()
-        userSelectedFont = self.fontPicker()
-        remove(self.tempFile)
-        return userSelectedFont
-
-    def writeFontsToTemporaryFile(self):
-        with open(self.tempFile , 'w') as file:
-            for font in self.fonts:
-                    file.writelines(f'{font.ancestor}\n')
-
-    def fontPicker(self):
-        fontMenu = subprocess.run(
-            ['cat', self.tempFile],
-            check=True,
-            text=True,
-            stdout=subprocess.PIPE
-        )
-        userFont = subprocess.run(
-            ['fzf'],
-            input = fontMenu.stdout,
-            check=True,
-            capture_output=True,
-            text=True
-        ).stdout.strip()
-        return userFont
-
-
 class TerminalRestart:
 
     def execute(self):
---
+++ DisplayFontPicker.py
@@ -0,0 +1,35 @@
+from os import path, remove, getcwd
+import subprocess
+
+class FontMenu:
+    def __init__(self, fontCollection):
+        self.fonts = fontCollection
+        self.tempFile = path.join(getcwd(), 'temporaryFile')
+
+    def letUserPickFont(self):
+        self.writeFontsToTemporaryFile()
+        userSelectedFont = self.fontPicker()
+        remove(self.tempFile)
+        return userSelectedFont
+
+    def writeFontsToTemporaryFile(self):
+        with open(self.tempFile , 'w') as file:
+            for font in self.fonts:
+                    file.writelines(f'{font.ancestor}\n')
+
+    def fontPicker(self):
+        fontMenu = subprocess.run(
+            ['cat', self.tempFile],
+            check=True,
+            text=True,
+            stdout=subprocess.PIPE
+        )
+        userFont = subprocess.run(
+            ['fzf'],
+            input = fontMenu.stdout,
+            check=True,
+            capture_output=True,
+            text=True
+        ).stdout.strip()
+        return userFont
+
